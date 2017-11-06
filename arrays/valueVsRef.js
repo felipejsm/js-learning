cee94e3f241daaa7eab2a7130614037d663ac295
Felipe Moysés <felipe.moyses@techne.com.br>
2017-11-06 18:07:31 -0200

    Cópia por referência e valor

--- arrays/valueVsRef.js
+++ arrays/valueVsRef.js
@@ -1,9 +1,27 @@
-var primeiroArray = ["UM","DOIS","TRES"];
-var segundoArray  = primeiroArray;
+// Criando um array, atribuindo seu valor a outro array
+let primeiroArray = ["UM","DOIS","TRES"];
+let segundoArray  = primeiroArray;
 
+
+// Alterando valor do 2º array
 segundoArray[1] = "QUATRO";
 
+
+//Valor do array original é alterado
+console.log("******************** Cópia por Referência ********************")
 console.log('Primeiro: '+primeiroArray);
-console.log('Segundo: '+segundoArray);
+console.log('Segundo:  '+segundoArray);
 console.log('São iguais? '+ (primeiroArray === segundoArray));
-alert(primeiroArray === segundoArray);
+//alert(primeiroArray === segundoArray);
+
+//Isso acontece porque o valor é passado por referência e não por valor(C-like?)
+
+segundoArray = primeiroArray.slice(0);
+
+segundoArray[1] = "CINCO";
+
+console.log("******************** Cópia por Valor ********************")
+console.log('Primeiro: '+primeiroArray);
+console.log('Segundo:  '+segundoArray);
+console.log('São iguais? '+ (primeiroArray === segundoArray));
+
