78eefdccc7586bb00d9cceeddd81c0fc75b21e05
Felipe Moysés <felipe.moyses@techne.com.br>
2017-11-07 10:14:21 -0200

    Trocando por Valor Array de Array

--- arrays/valueVsRef.js
+++ arrays/valueVsRef.js
@@ -26,7 +26,7 @@ console.log('Segundo:  '+segundoArray);
 console.log('São iguais? '+ (primeiroArray === segundoArray));
 
 let primeiroArrayDeArrays = [
-        ['Goku, Gohan, Vegita, Trunks, Future-Trunks'],
+        ['Goku', 'Gohan', 'Vegita', 'Trunks', 'Future-Trunks'],
         ['Yamcha','Kuririn','Yajirobe','Tenshinran','Mestre Kame'],
         ['Piccolo','Kami Sama','Dende','Senhor Popo'],
 ]
@@ -42,3 +42,20 @@ console.log("******************** Cópia por Ref Array de Arrays ***************
 console.log('Primeiro: '+primeiroArrayDeArrays[1]);
 console.log('Segundo: '+segundoArrayDeArrays[1]);
 
+//Para a cópia por referência surtir efeito, usa-se um laço for
+let primeiroArrayDeArrays2 = [
+        ['Goku', 'Gohan', 'Vegita', 'Trunks', 'Future-Trunks'],
+        ['Yamcha','Kuririn','Yajirobe','Tenshinran','Mestre Kame'],
+        ['Piccolo','Kami Sama','Dende','Senhor Popo'],
+];
+let segundoArrayDeArraysVazio = [];
+
+for(let index = 0, len = primeiroArrayDeArrays.length; index < len; index++) {
+        segundoArrayDeArraysVazio[index] = primeiroArrayDeArrays[index].slice();
+}
+// Trocando Gohan por Pan 
+segundoArrayDeArraysVazio[0][1] = 'Pan';
+
+console.log("******************** Cópia por Valor Array de Arrays ********************")
+console.log('Primeiro: '+primeiroArrayDeArrays[0]);
+console.log('Segundo: '+segundoArrayDeArraysVazio[0]);
